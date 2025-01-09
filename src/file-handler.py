abf76722c0ba2309d51328889dd56531788fb5c6
nouranali <nouranali.lumin@outlook.com>
2025-01-09 14:25:17 +0200

    add move files function

--- src/file-handler.py
+++ src/file-handler.py
@@ -17,9 +17,25 @@ class FileHandler:
             files = list(directory.rglob(extension))
             files=list(map(lambda file: f"File: {file.name}", files))
             pprint.pprint(f"files with {extension} extension:{files}")
+    def move_files(self,directory,extensions,destination):
+        """
+        Moves files in the specified directory with the given extensions to the destination directory.
 
+        Args:
+            directory (str or Path): The directory to search for files.
+            extensions (list of str): List of file extensions to search for.
+            destination (str or Path): The directory to move the files to.
+        """
+        directory = Path(directory)
+        destination = Path(destination)
+        for extension in extensions:
+            files = list(directory.rglob(extension))
+            for file in files:
+                file.rename(destination / file.name)
+                print(f"Moved {file.name} to {destination}")
 
 def main():
     filehandler= FileHandler()
     filehandler.list_files("/home/nouran/dl_utils/tmp",["*.json","*.txt"])
+    filehandler.move_files("/home/nouran/dl_utils/tmp",["*.txt"],"/home/nouran/dl_utils/tmp2")
 main()
