cb20d8fecd99a7c5edbb169935b0796c75f47f5b
nouranali <nouranali.lumin@outlook.com>
2025-01-09 14:59:22 +0200

    add mapper

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def make_parser():
     parser.add_argument('--destination_directory', type=str,nargs='+')
     parser.add_argument('--extensions', type=str,nargs='+')
     parser.add_argument('--move_files_by_extension', action='store_true')
+    parser.add_argument('--move_by_extension_and_directory', action='store_true')
     return parser
 
 def main():
@@ -13,15 +14,16 @@ def main():
     source_directory = args.source_directory
     destination_directory = args.destination_directory
     extensions = args.extensions
-    if len(destination_directory) == 1:
-        destination_directory = destination_directory[0]
     print(f"source_directory: {source_directory}")
     print(f"destination_directory: {destination_directory}")
     print(f"extensions: {extensions}")
     
     filehandler= FileHandler()
     filehandler.list_files(source_directory,extensions)
-    filehandler.move_files_by_extension(source_directory,extensions,destination_directory)
-    # filehandler.move_by_extension_and_directory("/home/nouran/dl_utils/tmp",{"*.txt":"/home/nouran/dl_utils/tmp2","*.json":"/home/nouran/dl_utils/tmp3"})
+    if args.move_files_by_extension:
+        filehandler.move_files_by_extension(source_directory,extensions,destination_directory)
+    if args.move_by_extension_and_directory:
+        mapper = dict(zip(extensions,destination_directory))
+        filehandler.move_by_extension_and_directory(source_directory,mapper)
 
 main()
