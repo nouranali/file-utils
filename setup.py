b5512caebf1c6de369e63e147b3b247a98fb1b60
nouranali <nouranali.lumin@outlook.com>
2025-01-11 13:14:32 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='file_utils',
+    version='0.0.1',
+    description='A file system util for ubuntu to handle moving and deletion using extensions',
+    author='Nouran Ali',
+    
+)
